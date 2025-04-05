2598414fe9b12ee17c3a93759ba525742f31b1d8
Yaotzinohell <akhileshb0906@gmail.com>
2025-04-06 00:27:58 +0530

    Solved Search in Rotated Sorted Array

---
+++ Search_in_Rotated_Sorted_Array/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if target in nums:
+            pos=nums.index(target)
+            return pos
+        else:
+            return -1
