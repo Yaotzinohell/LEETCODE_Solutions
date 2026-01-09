c72fb83879bf63de31106f1273e4f754b9ec3005
Yaotzinohell <akhileshb0906@gmail.com>
2026-01-10 01:47:12 +0530

    Still havent got a job

---
+++ Leetcode_326/solution.py
@@ -0,0 +1,14 @@
+import math
+class Solution(object):
+    def isPowerOfThree(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n<=0:
+            return False
+        if n%3==0:
+            return self.isPowerOfThree(n//3)
+        if n==1:
+            return True
+        return False
