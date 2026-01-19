4de035e425462bf0469c89ba9bf9d9bc82b50d3d
Yaotzinohell <akhileshb0906@gmail.com>
2026-01-20 02:00:53 +0530

    Did a few more qns

---
+++ Leetcode_242/solution.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)!=len(t):
+            return False
+        else:
+            for ch in s:
+                if s.count(ch)!=t.count(ch):
+                    return False
+        
+        return True
